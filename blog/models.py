07e50a8828fd3c8069c3be8aa2887b646e90d41e
sam <sam.cormack@gmail.com>
2019-09-21 21:31:18 +1200

    Basic blog created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.search import index
+
+
+class BlogIndexPage(Page):
+    content_panels = Page.content_panels
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = StreamField([
+        ('heading', blocks.CharBlock()),
+        ('text', blocks.RichTextBlock()),
+    ])
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        StreamFieldPanel('body'),
+    ]
